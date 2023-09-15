c0964a8e75d437c61b0838fba6a7549e80df4be2
Lucid <100102668+dearmysolitude@users.noreply.github.com>
2023-09-15 17:58:22 +0900

    Spring Security deactivated

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -1,6 +1,9 @@
+
+#Crawl current port of WAS
 CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
+# Toggle Port Number
 if [ ${CURRENT_PORT} -eq 8081 ]; then
   TARGET_PORT=8082
 elif [ ${CURRENT_PORT} -eq 8082 ]; then
